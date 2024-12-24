3f449fbfdf9a7d593e11e27543bb7c243d85b99c
Arunkumar Mourougappane <amouroug@buffalo.edu>
2024-12-24 13:15:24 -0600

    Enable debug logs and replace some of the serial println with debug logs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ void setup()
 {
 
    Serial.begin(115200);
+   Serial.setDebugOutput(true);
    Serial.println();
    pinMode(LED_BUILTIN, OUTPUT);
 
@@ -40,8 +41,7 @@ void loop()
 
 void Task0code(void *pvParameters)
 {
-   Serial.print("Task0 running on core ");
-   Serial.println(xPortGetCoreID());
+   log_i("Task0 running on core %d\n", xPortGetCoreID());
    // Setup 6DOF Data
    delay(2000);
    Adafruit_LSM6DSOX lsm6dsox;
@@ -51,11 +51,11 @@ void Task0code(void *pvParameters)
    i2c_wire.setPins(LSM6DOX_SDA_PIN, LSM6DOX_SCL_PIN);
    while (!lsm6dsox.begin_I2C(LSM6DS_I2CADDR_DEFAULT, &i2c_wire, 0))
    {
-      Serial.println("Failed to initialize LSM6DOX.");
+      log_e("Failed to initialize LSM6DOX.");
       delay(1000);
    }
 
-   Serial.println("LSM6DSOX Found!");
+   log_i("LSM6DSOX Found!");
 
    sensors_event_t accel;
    sensors_event_t gyro;
@@ -81,12 +81,14 @@ void Task0code(void *pvParameters)
          led_wait_count++;
       }
       lsm6dsox.getEvent(&accel, &gyro, &temp);
+#ifdef TELEPLOT_ENABLE
       Serial.printf(">AccX:%0.2f\n", accel.acceleration.x);
       Serial.printf(">AccY:%0.2f\n", accel.acceleration.y);
       Serial.printf(">AccZ:%0.2f\n", accel.acceleration.z);
       Serial.printf(">GyroX:%0.2f\n", gyro.gyro.x);
       Serial.printf(">GyroY:%0.2f\n", gyro.gyro.y);
       Serial.printf(">GyroZ:%0.2f\n", gyro.gyro.z);
+#endif
       pixels.UpdatePixelColor(CNeoPixel::Color(0, 0, 0)); // Set pixel to red
       delay(100);
    }
@@ -94,11 +96,13 @@ void Task0code(void *pvParameters)
 
 void toggleOff() {
    digitalWrite(BUILTIN_LED, LOW);
+   log_i("Led Toggled OFF");
    server.send(200, "text/plain", "Ok");
 }
 
 void toggleOn() {
    digitalWrite(BUILTIN_LED, HIGH);
+   log_i("Led Toggled ON\n");
    server.send(200, "text/plain", "Ok");
 }
 
@@ -109,13 +113,13 @@ void handleNotFound()
 
 void Task1code(void *pvParameters)
 {
-   Serial.print("Task1 running on core ");
-   Serial.println(xPortGetCoreID());
-   Serial.println("Server started");
+   log_i("Task1 running on core %d", xPortGetCoreID());
+   log_i("Setting up Web server handlers");
    server.on("/on", toggleOn);
    server.on("/off", toggleOff);
    server.onNotFound(handleNotFound);
    server.begin();
+   log_i("Starting Web Server");
    for (;;)
    {
       server.handleClient();
